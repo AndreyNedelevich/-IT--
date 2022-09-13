3c68f030807efbb30ff69f6f2d5dbb1b26dbb223
AndreyNedelevich <113480738+AndreyNedelevich@users.noreply.github.com>
2022-09-14 02:19:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const bitcoinPrice = document.querySelector("#current--1");
+const money = document.querySelector("#current--2");
+const btn = document.querySelector("#btn");
+const resaltExchaning = document.querySelector("#sum");
+
+const exchange = function () {
+  if (bitcoinPrice.value <= 0 && money.value <= 0) {
+    return alert("Invalid number");
+  }
+
+  resaltExchaning.textContent = `${+money.value / +bitcoinPrice.value} BTC`;
+};
+
+btn.addEventListener("click", exchange);
